97d7344012aa7fafd867ad2615a8d817ad3764b5
Hilman Aditya <12210686@bsi.ac.id>
2024-06-22 17:19:00 +0700

    update fitur contact app

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
-// import { tulisPertanyaan, simpanContact } from './contacts';
 import yargs from 'yargs';
-// mengambil argument dari command line
-console.log(yargs.argv);
-
-
-// const main = async () => {
-//     const nama = await tulisPertanyaan('Masukkan nama anda: ');
-//     const email = await tulisPertanyaan('Masukkan email anda: ');
-
-//     simpanContact(nama, email);
-// };
-
-// main();
+import { hideBin } from 'yargs/helpers';
+import { simpanContact } from './contacts';
+// Ambil argument dari command line
+yargs(hideBin(process.argv))
+    .command({
+        command: 'add',
+        describe: 'Menambahkan Contact Baru',
+        builder: { //  digunakan untuk mendefinisikan argumen yang dibutuhkan
+            nama: {
+                describe: 'Nama Lengkap',
+                demandOption: true,
+                type: 'string'
+            },
+            noHp: {
+                describe: 'Nomor Telepon',
+                demandOption: true,
+                type: 'string'
+            },
+            email: {
+                describe: 'Email',
+                demandOption: false,
+                type: 'string'
+            }
+        },
+        handler(argv) { // fungsi yang dijalankan ketika perintah dipanggil
+            simpanContact(argv.nama, argv.noHp, argv.email);
+        },
+    })
+    .help()
+    .parse(); // Mem-parsing argumen
--- contacts.js
+++ contacts.js
@@ -1,36 +1,48 @@
+import chalk from 'chalk';
 import fs from 'fs';
-import readline from 'readline';
+import validator from 'validator';
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
+// membuat folder data jika belum ada
 const dirPath = ('./data/contacts.json');
 if (!fs.existsSync(dirPath)) {
     fs.mkdirSync(dirPath);
 }
+
 
 const dataPath = ('./data/contacts.json');
 if (!fs.existsSync(dataPath)) {
     fs.writeFileSync(dataPath, '[]', 'utf-8 ');
 }
 
-export const tulisPertanyaan = (pertanyaan) => {
-    return new Promise((resolve, rejects) => {
-        rl.question(pertanyaan, (nama) => {
-            resolve(nama);
-        });
-    });
-};
 
-export const simpanContact = (nama, email) => {
-    const contact = { nama, email };
+export const simpanContact = (nama, noHp, email) => {
+    const contact = { nama, noHp, email };
     const file = fs.readFileSync('./data/contacts.json', 'utf8');
     const contacts = JSON.parse(file);
+
+    // cek duplikat
+    const duplikat = contacts.find((contact) => contact.nama === nama);
+    if (duplikat) {
+        console.log(chalk.red.inverse.bold('Contact sudah terdaftar, gunakan nama lain'));
+        return false;
+    }
+
+    // cek email
+    if (email) {
+        if (!validator.isEmail(email)) {
+            console.log(chalk.red.inverse.bold('Email tidak valid'));
+            return false;
+        }
+    }
+
+    // cek nomor HP
+    if (!validator.isMobilePhone(noHp, 'id-ID')) {
+        console.log(chalk.red.inverse.bold('Nomor Hp tidak valid'));
+        return false;
+    }
     contacts.push(contact);
 
+
     fs.writeFileSync('./data/contacts.json', JSON.stringify(contacts));
-    console.log('Terimakasih sudah mengisi data');
-    rl.close();
+    console.log(chalk.green.inverse.bold('Terimakasih sudah mengisi data'));
 };
